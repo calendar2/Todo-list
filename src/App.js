373ecd7070f3fcfd65c2961d01f0beffc440515e
calendar2 <zoro94317@gmail.com>
2023-08-31 23:58:11 +0900

    8/31 투두리스트 css가 너무 어렵다...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Header from './contents/Header';
-import Nav from './contents/Nav';
-import Patrick from './contents/Patrick';
 
-function App(props) {
-  const [todos, setTodos] = useState([]);
+function App() {
+  const [title, setTitle] = useState('');
+  const [contents, setContents] = useState('');
 
-  // const onAddList = (title, contents) => {
-  //   const newTodo = {'title' : title, 'contents' : contents}
-  //   const newTodos = [...todos]
-  //   newTodos.push(newTodo)
-  //   setTodos(newTodos)
-  //   console.log(todos)
-  // }
+  const [todoList, setTodoList] = useState([]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if(title === '') {
+      alert('제목을 입력해주세요!');
+    }
+    else if(contents === '') {
+      alert('내용을 입력해주세요!');
+    }
+    else {
+      const nextTodoList = todoList.concat({
+        id: todoList.length,
+        title: title,
+        contents: contents,
+        checked: false,
+      })
+
+      setTodoList(nextTodoList)
+      setTitle('');
+      setContents('');
+    }
+  }
+
+  const onDelete = (id) => {
+    setTodoList(todoList.filter(todoItem =>
+      todoItem.id !== id
+    ));
+  };
+
+  const onDone = (id) => {
+    setTodoList(todoList.map(todoItem =>
+      todoItem.id === id ?
+      {...todoItem, checked : !todoItem.checked} : todoItem
+    ))
+  }
 
   return (
-    <div className="App">
-      <Header/>
-      <Nav setTodos={setTodos} />
-      <Patrick/>
-      <div>{todos}</div>
+    <div>
+      {/* Head 영역 */}
+      <div className='header'>
+        <h1>Todo-List</h1>
+      </div>
+
+      {/* 작성 영역 */}
+      <div className="container">
+        <div className='addbox'>
+          <div>
+            <div>Title</div>
+            <div>Contents</div>
+          </div>
+          <div>
+            <form onSubmit={onSubmit}>
+              <input className='inputTitle' type='text' value={title} onChange={(e) => setTitle(e.target.value)} />
+              <input className='inputContents' type='text' value={contents} onChange={(e) => setContents(e.target.value)} />
+              <input className='inputSubmit' type="submit" value='ADD' />
+            </form>
+          </div>
+        </div>
+      </div>
+
+      {/* 목록 영역 */}
+      <div className='lists'>
+        {todoList.map((todoItem) => (
+          <div className='container2'>
+            <div className='contentList'>
+              <input className='checkbox' type='checkbox' checked={todoItem.checked} />
+              {todoItem.checked ? 'Done !' : ''}
+              <br/>
+              <h2>{todoItem.title}</h2>
+              {todoItem.contents}
+              <br/><br/><br/>
+              <button type='button' className='deleteBtn' onClick={() => onDelete(todoItem.id)}>DELETE</button>
+              {todoItem.checked ?
+              <button type='button' className='doneBtn' onClick={() => onDone(todoItem.id)}>CANCEL</button> :
+              <button type='button' className='doneBtn' onClick={() => onDone(todoItem.id)}>DONE</button>
+              }
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/contents/Nav.js
+++ src/contents/Nav.js
@@ -1,35 +1,42 @@
 import React , { useState } from "react";
 
 function Nav(props) {
-  const [title, setTitle] = useState('');
-  const [contents, setContents] = useState('');
+  const [title_nav, setTitle_nav] = useState('');
+  const [contents_nav, setContents_nav] = useState('');
+
+  const [todos, setTodos] = useState([]);
 
   const onAdd = (e) => {
     e.preventDefault();
 
-    if(title === '') {
+    if(title_nav === '') {
       alert('제목을 입력해주세요!');
     }
-    else if(contents === '') {
+    else if(contents_nav === '') {
       alert('내용을 입력해주세요!');
     }
     else {
-      const newTodo = {'title' : title, 'contents' : contents};
-      const newTodos = [...props.todos]
-      newTodos.push(newTodo)
-      props.setTodos(newTodos);
+      props.setTitle(title_nav);
+      props.setContents(contents_nav)
+
+      const newTodo = {title_nav : title_nav, contents_nav : contents_nav};
+      const newTodos = [...todos];
+      newTodos.push(newTodo);
+      setTodos(newTodos);
+
+      console.log(todos[0].title_nav)
     }
     
-    setTitle('');
-    setContents('');
+    setTitle_nav('');
+    setContents_nav('');
   }
 
   return (
     <nav>
       Title
-      <input type='text' value={title} onChange={(e) => setTitle(e.target.value)} />
+      <input type='text' value={title_nav} onChange={(e) => setTitle_nav(e.target.value)} />
       Contents
-      <input type='text' value={contents} onChange={(e) => setContents(e.target.value)} />
+      <input type='text' value={contents_nav} onChange={(e) => setContents_nav(e.target.value)} />
       <button type="button" onClick={onAdd}>ADD</button>
     </nav>
   );
